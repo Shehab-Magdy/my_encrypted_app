889b04652d663d8fdb062d43487cce2318879002
Shehab Magdy El-Adl <cegres1@hotmail.com>
2024-02-11 18:34:57 +0200

    adding ignore file

--- lic_creator.py
+++ lic_creator.py
@@ -5,8 +5,8 @@ from glob import glob
 from zipfile import ZipFile
 
 
-period = 30             # days
 clientName = input('Please Type Client name: ')
+period = int(input('Please Type Period in days: '))
 licenseFileName = 'License.lic'
 publicKey =rsa.PublicKey(10871363084898070022303896002903391265444392016308981589701418203435182379057843234360336520869928895398932205768274281413654263140154242008097101032149633, 65537)
 timeNow = datetime.now()
@@ -15,9 +15,7 @@ fileName = clientName + '_' + str(period) + '.zip'
 format_string = '%Y-%m-%d %H:%M:%S'
 
 message = end_date.strftime(format_string)
-# publicKey, privateKey = rsa.newkeys(512)
 encMessage = rsa.encrypt(message.encode(), publicKey)
-# wMessage = str(encMessage)
 
 for filePath in glob('*.zip'):
     if os.path.isfile(filePath):
@@ -30,4 +28,4 @@ zf = ZipFile(fileName, 'w')
 zf.write(licenseFileName)
 zf.close()
 
-# os.remove(licenseFileName)
+os.remove(licenseFileName)
