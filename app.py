b0853ecb1ee04438d71afe0b42fe22c4a0593743
Shehab Magdy El-Adl <cegres1@hotmail.com>
2024-02-11 16:05:44 +0200

    code for time

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 import rsa
 
+
+
+
+
+# decMessage = rsa.decrypt(encMessage, privateKey).decode()
+
+# print("decrypted string: ", decMessage)
--- lic_creator.py
+++ lic_creator.py
@@ -1,2 +1,13 @@
+from datetime import datetime, timedelta
 import rsa
+ 
+
+period = 30             # days
+timeNow  = datetime.now()
+end_date = timeNow + timedelta(days=period)
+
+message = str(end_date)
+
+print("original string: ", message)
+print("encrypted string: ", encMessage)
 
