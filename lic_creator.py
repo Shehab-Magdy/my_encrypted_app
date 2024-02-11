48971d83f29d904d84cc3c3c6f5bd8dc5e915521
Shehab Magdy El-Adl <cegres1@hotmail.com>
2024-02-11 16:37:09 +0200

    creating zip file with the nam eof the client contains lic file

--- lic_creator.py
+++ lic_creator.py
@@ -2,11 +2,12 @@ from datetime import datetime, timedelta
 import rsa
 import os
 import glob
+import zipfile
  
 
 period = 30             # days
 clientName = input('Please Type Client name: ')
-fileName = clientName + '_' + str(period)
+fileName = clientName + '_' + str(period) + '.zip'
 
 timeNow  = datetime.now()
 end_date = timeNow + timedelta(days=period)
@@ -19,10 +20,16 @@ encMessage = rsa.encrypt(message.encode(), publicKey)
 wMessage = str(encMessage)
 
 
-for filePath in glob.glob('*.lic'):
+for filePath in glob.glob('*.zip'):
     if os.path.isfile(filePath):
         os.remove(filePath)
 
-f = open(fileName, "w")
+f = open('License.lic', "w")
 f.write(wMessage)
 f.close()
+
+zf = zipfile.ZipFile(fileName,'w')
+zf.write('License.lic')
+zf.close()
+
+os.remove('License.lic')
