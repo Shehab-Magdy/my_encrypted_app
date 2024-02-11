57d6bd8acf771daf017e0d9786e1739f97716e30
Shehab Magdy El-Adl <cegres1@hotmail.com>
2024-02-11 17:29:35 +0200

    reading the lic file

--- lic_creator.py
+++ lic_creator.py
@@ -1,35 +1,32 @@
 from datetime import datetime, timedelta
 import rsa
 import os
-import glob
-import zipfile
- 
+from glob import glob
+from zipfile import ZipFile
+
 
 period = 30             # days
 clientName = input('Please Type Client name: ')
+licenseFileName = 'License.lic'
+publicKey =rsa.PublicKey(10871363084898070022303896002903391265444392016308981589701418203435182379057843234360336520869928895398932205768274281413654263140154242008097101032149633, 65537)
+timeNow = datetime.now()
+end_date = timeNow + timedelta(days=period)
 fileName = clientName + '_' + str(period) + '.zip'
 
-timeNow  = datetime.now()
-end_date = timeNow + timedelta(days=period)
-
 message = str(end_date)
+# publicKey, privateKey = rsa.newkeys(512)
+encMessage = rsa.encrypt(message.encode('utf-8'), publicKey)
+# wMessage = str(encMessage)
 
-publicKey, privateKey = rsa.newkeys(512)
-encMessage = rsa.encrypt(message.encode(), publicKey)
-
-wMessage = str(encMessage)
-
-
-for filePath in glob.glob('*.zip'):
+for filePath in glob('*.zip'):
     if os.path.isfile(filePath):
         os.remove(filePath)
 
-f = open('License.lic', "w")
-f.write(wMessage)
-f.close()
+with open(licenseFileName, 'wb') as f:
+        f.write(encMessage)
 
-zf = zipfile.ZipFile(fileName,'w')
-zf.write('License.lic')
+zf = ZipFile(fileName, 'w')
+zf.write(licenseFileName)
 zf.close()
 
-os.remove('License.lic')
+# os.remove(licenseFileName)
