7fc7b50faaafab24586be8e1ddb2b8607c3d8857
Shehab Magdy El-Adl <cegres1@hotmail.com>
2024-02-11 16:25:41 +0200

    creating lic file with the name of the client adding his period

--- lic_creator.py
+++ lic_creator.py
@@ -1,8 +1,13 @@
 from datetime import datetime, timedelta
 import rsa
+import os
+import glob
  
 
 period = 30             # days
+clientName = input('Please Type Client name: ')
+fileName = clientName + '_' + str(period)
+
 timeNow  = datetime.now()
 end_date = timeNow + timedelta(days=period)
 
@@ -11,6 +16,13 @@ message = str(end_date)
 publicKey, privateKey = rsa.newkeys(512)
 encMessage = rsa.encrypt(message.encode(), publicKey)
 
-print("original string: ", message)
-print("encrypted string: ", encMessage)
+wMessage = str(encMessage)
 
+
+for filePath in glob.glob('*.lic'):
+    if os.path.isfile(filePath):
+        os.remove(filePath)
+
+f = open(fileName, "w")
+f.write(wMessage)
+f.close()
